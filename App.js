e1be0bed1aca3acfd7b98a81bbe87ff499f7acd6
sisoalbert <sisoalbert@yahoo.com>
2022-04-17 14:23:51 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+import RootNavigator from "./src/navigation/RootNavigator";
+
+//Redux
+import { Provider } from "react-redux";
+import { store, appPersist } from "./src/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={appPersist}>
+        <RootNavigator />
+      </PersistGate>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
